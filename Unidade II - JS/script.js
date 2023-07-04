c37421b6a30b36f1934927500fe787036da1d742
Veridiano <veridianobarroso@gmail.com>
2023-07-04 08:59:48 -0500

    js

---
+++ Unidade II - JS/script.js
@@ -0,0 +1,24 @@
+// Declaração de variáveis
+var nome = "João";
+var idade = 25;
+var altura = 1.75;
+var estudante = true;
+
+// Exibindo os valores das variáveis no console
+console.log("Nome: " + nome);
+console.log("Idade: " + idade);
+console.log("Altura: " + altura);
+console.log("Estudante: " + estudante);
+
+// Realizando operações com as variáveis
+var idadeDaquiACincoAnos = idade + 5;
+var alturaEmMetros = altura * 100;
+
+// Exibindo os resultados das operações no console
+console.log("Daqui a cinco anos, a idade será: " + idadeDaquiACincoAnos);
+console.log("Altura em centímetros: " + alturaEmMetros);
+
+
+// Neste exemplo, declaramos quatro variáveis: nome, idade, altura e estudante. Em seguida, exibimos seus valores utilizando a função console.log().
+
+// Após isso, realizamos algumas operações com as variáveis, como somar um valor à idade e multiplicar a altura por 100. Novamente, utilizamos console.log() para exibir os resultados.
